41b8a9160a2f1101906abf2ac1ef41b72d08db2e
GustavoQueiros <gustavo.dev47@gmail.com>
2025-03-06 15:14:47 -0300

    feat: "Simple fix in main"

---
+++ pipeline-jenkins/src/main/java/com/queiros/pipeline_jenkins/PipelineJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.queiros.pipeline_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineJenkinsApplication.class, args);
+	}
+
+}
+
---
+++ pipeline-jenkins/src/test/java/com/queiros/pipeline_jenkins/PipelineJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queiros.pipeline_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
